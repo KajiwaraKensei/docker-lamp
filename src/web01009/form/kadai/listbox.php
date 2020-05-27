345cd2f22fbe689666637df052fd613f2fb2ce5b
KajiwaraKensei <kd1270632@st.kobedenshi.ac.jp>
2020-05-27 14:04:14 +0900

    listboxとその他

--- src/web01009/form/kadai/listbox.php
+++ src/web01009/form/kadai/listbox.php
@@ -5,18 +5,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>checkbox.php</title>
+  <title>リストボックス</title>
 </head>
 
 <body>
   <h4>1組 9番 梶原健成</h4>
-  <p>ラジオボタン</p>
-  <?='あなたの好きな果物は、'?><?php
+  <div>
+  <?='あなたの好きなフルーツは、'?><?php
 foreach ((filter_input(INPUT_POST, "fruits", FILTER_DEFAULT, ['flags' => FILTER_REQUIRE_ARRAY]) ?: ["不明"]) as $fruit) {
     echo $fruit . ' ';
 }
 ?>
-  <?='ですね。'?>
-  <a href="checkbox.html">戻る</a>
+ <?='ですね。'?>
+  </div>
+  <a href="listbox.html">戻る</a>
 </body>
+
 </html>
