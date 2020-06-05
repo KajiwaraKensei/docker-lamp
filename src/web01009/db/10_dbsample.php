a30bf1dedc3b51da479445b7cfb8a784928ae29d
KajiwaraKensei <kd1270632@st.kobedenshi.ac.jp>
2020-06-05 10:58:32 +0900

    db 課題

---
+++ src/web01009/db/10_dbsample.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>dbsample.php</title>
+</head>
+
+<body><br><?php
+// $dsn = 'mysql:host=localhost;dbname=php;charset=utf8';
+$dsn = 'mysql:host=mysql;dbname=php;charset=utf8';
+$user = 'kobe';
+$password = 'denshi';
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    $sql = 'select * from company';
+    $stmt = $pdo->query($sql);
+    $results = $stmt->fetchAll();
+
+    foreach ($results as $result) {
+        echo 'cid：' . $result['cid'] . ', 会社名：' . $result['name'] . ', 住所：' . $result['address'] . '<br>';
+    }
+} catch (Exception $e) {
+    echo 'Error:' . $e->getMessage();
+    die();
+}
+$pdo = null;
+?>
+  <hr>
+  <h4>○○組　□□□番　神戸電子</h4>
+</body>
+
+</html>
