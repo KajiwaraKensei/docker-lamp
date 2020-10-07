00492c55c43fe6d4e36b9234e16c2cdda534653d
KajiwaraKensei <kd1270632@st.kobedenshi.ac.jp>
2020-10-07 14:22:26 +0900

    6-2

--- src/shop01009/cart/cart_list.php
+++ src/shop01009/cart/cart_list.php
@@ -3,59 +3,59 @@
 <head>
 <meta charset="UTF-8">
 <title>カート</title>
-<link rel="stylesheet" href="../css/minishop.css">
+<link rel="stylesheet" href="../css/shop.css">
 </head>
 <body>
 <h3>カート内の商品</h3>
-○○組　□□□番　神戸電子<br>
+01組　009番　梶原健成<br>
 <hr>
 <?php
-  // カート内のすべてのデータを取り出す
-  $cartItems = $cart->getItems();
-  if(empty($cartItems)){
+// カート内のすべてのデータを取り出す
+$cartItems = $cart->getItems();
+if (empty($cartItems)) {
     echo '<h4>お客様のショッピングカートに商品はございません。</h4>';
     echo '<a href="../index.php">ジャンル選択に戻る</a>';
-  } else {
-?>
+} else {
+    ?>
 <table>
 <tr><th>&nbsp;</th><th>商品名</th><th>メーカー・著者<br>アーティスト</th><th>価格</th><th>注文数</th><th>金額</th><th>削除</th></tr>
 <?php
-  $total = 0;
-  foreach($cartItems as $item){
-    $total += $item['price'] * $item['quantity'];
-?>
-    <tr><td class="td_mini_img"><img class="mini_img" src="../images/<?= $item['image'] ?>"></td>
-    <td class="td_item_name"><?= $item['name'] ?></td> 
-    <td class="td_item_maker"><?= $item['maker'] ?></td> 
-    <td class="td_right">&yen;<?= number_format($item['price']) ?></td>
+$total = 0;
+    foreach ($cartItems as $item) {
+        $total += $item['price'] * $item['quantity'];
+        ?>
+    <tr><td class="td_mini_img"><img class="mini_img" src="../images/<?=$item['image']?>"></td>
+    <td class="td_item_name"><?=$item['name']?></td>
+    <td class="td_item_maker"><?=$item['maker']?></td>
+    <td class="td_right">&yen;<?=number_format($item['price'])?></td>
     <td><form method="POST" action="cart_change.php">
         <select name="quantity">
 <?php
-        for($i=1; $i<=10; $i++){
-          echo '<option value="' . $i . '"';
-          if($i == $item['quantity']){
-            echo ' selected>';
-          } else {
-            echo '>';
-          }
-          echo $i . '</option>';
+for ($i = 1; $i <= 10; $i++) {
+            echo '<option value="' . $i . '"';
+            if ($i == $item['quantity']) {
+                echo ' selected>';
+            } else {
+                echo '>';
+            }
+            echo $i . '</option>';
         }
-?>
+        ?>
         </select>
-        <input type="hidden" name="ident" value="<?= $item['ident'] ?>">&nbsp; 
+        <input type="hidden" name="ident" value="<?=$item['ident']?>">&nbsp;
         <input type="submit" value="変更">
     </form></td>
-    <td class="td_right">&yen;<?= number_format($item['price'] * $item['quantity']) ?></td>
-    <td><form method="POST" action="cart_delete.php"> 
-        <input type="hidden" name="ident" value="<?= $item['ident'] ?>">
+    <td class="td_right">&yen;<?=number_format($item['price'] * $item['quantity'])?></td>
+    <td><form method="POST" action="cart_delete.php">
+        <input type="hidden" name="ident" value="<?=$item['ident']?>">
         <input type="submit" value="削除">
     </form></td>
     </tr>
 <?php
-  }
-?>
+}
+    ?>
 <tr><th colspan="5">合計金額</th>
-    <td class="td_right">&yen;<?= number_format($total) ?></td>
+    <td class="td_right">&yen;<?=number_format($total)?></td>
     <td>
       <form method="POST" action="cart_clear.php">
         <input type="submit" value="全て">
@@ -66,7 +66,7 @@
 <br>
 <a href="../index.php">ジャンル選択に戻る</a>&nbsp;&nbsp;<a href="../order/order_now.php">注文する</a>
 <?php
-  }
+}
 ?>
 </body>
 </html>
--- src/shop01009/order/order_now.php
+++ src/shop01009/order/order_now.php
@@ -1,18 +1,18 @@
 <?php
-  // Cartオブジェクトを生成する
-  require_once __DIR__ . '/../classes/cart.php';
-  $cart = new Cart();
+// Cartオブジェクトを生成する
+require_once __DIR__ . '/../classes/cart.php';
+$cart = new Cart();
 
-  // カート内の全ての商品を取り出す
-  $cartItems = $cart->getItems();
+// カート内の全ての商品を取り出す
+$cartItems = $cart->getItems();
 
-  // Orderオブジェクトを生成する
-  require_once __DIR__ . '/../classes/order.php';
-  $order = new Order();
+// Orderオブジェクトを生成する
+require_once __DIR__ . '/../classes/order.php';
+$order = new Order();
 
-  // カート内の全ての商品を注文内容として登録する
-  // 注文テーブルordersと注文詳細テーブルorderdetailsに注文内容を登録する
-  $orderId = $order->addOrder($cartItems);
+// カート内の全ての商品を注文内容として登録する
+// 注文テーブルordersと注文詳細テーブルorderdetailsに注文内容を登録する
+$orderId = $order->addOrder($cartItems);
 ?>
 <!DOCTYPE  html>
 <html lang="ja">
@@ -23,37 +23,37 @@
 </head>
 <body>
 <h3>注文</h3>
-○○組　□□□番　神戸電子<br>
+01組　009番　梶原健成<br>
 <hr>
 <p>お買い上げありがとうございました。<br>
 またのご利用をお待ちしております。</p>
 <table>
 <tr><th>&nbsp;</th><th>商品名</th><th>メーカー・著者<br>アーティスト</th><th>価格</th><th>注文数</th><th>金額</th></tr>
 <?php
-  $total = 0;
-  foreach($cartItems as $item){
+$total = 0;
+foreach ($cartItems as $item) {
     $total += $item['price'] * $item['quantity'];
-?>
+    ?>
     <tr>
-    <td class="td_mini_img"><img class="mini_img" src="../images/<?= $item['image'] ?>"></td>
-    <td class="td_item_name"><?= $item['name'] ?></td>
-    <td class="td_item_maker"><?= $item['maker'] ?></td>
-    <td class="td_right">&yen;<?= number_format($item['price']) ?></td>
-    <td class="td_right"><?= $item['quantity'] ?></td>
-    <td class="td_right">&yen;<?= number_format($item['price'] * $item['quantity']) ?></td>
+    <td class="td_mini_img"><img class="mini_img" src="../images/<?=$item['image']?>"></td>
+    <td class="td_item_name"><?=$item['name']?></td>
+    <td class="td_item_maker"><?=$item['maker']?></td>
+    <td class="td_right">&yen;<?=number_format($item['price'])?></td>
+    <td class="td_right"><?=$item['quantity']?></td>
+    <td class="td_right">&yen;<?=number_format($item['price'] * $item['quantity'])?></td>
     </tr>
 <?php
-  }
+}
 ?>
 <tr><th colspan="5">合計金額</th>
-<td class="td_right">&yen;<?= number_format($total) ?></td>
+<td class="td_right">&yen;<?=number_format($total)?></td>
 </tr>
 </table>
 <br>
 <a href="../index.php">ジャンル選択に戻る</a>&nbsp;&nbsp;<a href="order_history.php">注文履歴</a>
 <?php
-  // カート内の全ての商品を削除する
-  $cart->clearCart();
+// カート内の全ての商品を削除する
+$cart->clearCart();
 ?>
 </body>
 </html>
